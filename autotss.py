767f7a09f5ae36b320e488d856fa1796ffc6fc6c
codsane <adam@Adams-iMac.local>
2018-01-24 11:31:33 -0600

    Addresses TypeError in #2, tsschecker binary moved for clarity
    Addresses TypeError in #2
    - Fixed an issue where liveFirmwareAPI was being called before
    being assigned
    
    Addresses no blobs saved in #4
    - Detection to see if blobs were actually saved
    - Now logs tsschecker’s output and saves upon error, for debugging
    
    Moved the tsschecker binary location
    - The tsschecker.zip should now be unzipped and folder placed in
    the same directory as autotss.py
    - Script will now look for both /tsschecker and /tsschecker-latest
    for binary
    - Grabs the correct binary depending on operating system
    - Checks to make sure tsschecker binaries exist before proceeding
    
    Added the ability to manually specify path to the tsschecker binary
    - Path to your tsschecker binary can be passed as a parameter to
    autotss() or as command line argument “-p” (see readme/usage)
    - Checks to make sure tsschecker binaries exist before proceeding
    
    Cleaned up some printing
    - No longer prints when a device’s blobs have already been saved
    (uncomment line #133 to reverse this)
    - Universally uses device name rather than device id for printing
    
    Moved the location of saved blobs to /blobs/device model/device
    ecid/version number/version build
    - Found that there are multiple iOS builds with the same version
    number, which required us to move blobs inside a version build folder
    
    Remanining known issues
    - Weird results from tsschecker when saving iOS 11.2 Build 15C113
    blobs (Build 15C114 blobs functions normally)

--- autotss.py
+++ autotss.py
@@ -1,191 +1,281 @@
 import requests as r
 import configparser
 import subprocess
+import argparse
 import dataset
 import json
+import sys
 import os
 import io
 
 
 class autotss:
 
-    def __init__(self):
-        self.liveFirmwareURL = 'https://api.ipsw.me/v2.1/firmwares.json/condensed'
-        self.liveFirmwareAPI = None
-        # self.otaFirmwareURL = 'https://api.ipsw.me/v2.1/ota.json/condensed'
-        # self.otaFirmwareAPI = None
-        self.database = dataset.connect('sqlite:///autotss.db')
+	def __init__(self, userPath = None):
+		self.scriptPath = self.getScriptPath(userPath)
+		self.liveFirmwareAPI = self.getFirmwareAPI()
+		self.database = dataset.connect('sqlite:///autotss.db')
 
-        self.importNewDevices()
-        self.devices = [row for row in self.database['devices']]
-        self.checkAllDevices()
-        self.pushToDatabase()
+		self.importNewDevices()
+		self.checkAllDevices()
+		self.pushToDatabase()
 
-    def importNewDevices(self):
-        """ Checks devices.txt for new entries. Parses entries and
-        inserts them into the devices table in our database """
+	def importNewDevices(self):
+		""" Checks devices.txt for new entries. Parses entries and
+		inserts them into the devices table in our database """
 
-        print('Checking devices.ini for new devices...')
-        db = self.database['devices']
-        newDevices = []
-        numNew = 0
+		print('\nChecking devices.ini for new devices...')
+		db = self.database['devices']
+		newDevices = []
+		numNew = 0
 
-        # Check to make sure devices.ini exists, otherwise warn and continue without new devices
-        if os.path.isfile('devices.ini'):
-            config = configparser.ConfigParser()
-            config.read('devices.ini')
-            for section in config.sections():
-                name = section
-                identifier = config.get(section, 'identifier').replace(' ','')
-                ecid = config.get(section, 'ecid')
+		# Check to make sure devices.ini exists, otherwise warn and continue without new devices
+		if os.path.isfile('devices.ini'):
+			config = configparser.ConfigParser()
+			config.read('devices.ini')
+			for section in config.sections():
+				name = section
+				identifier = config.get(section, 'identifier').replace(' ','')
+				ecid = config.get(section, 'ecid')
 
-                try:
-                    boardconfig = config.get(section, 'boardconfig')
-                except:
-                    boardconfig = ''
-                if not boardconfig:
-                    boardconfig = self.getBoardConfig(identifier)
+				try:
+					boardconfig = config.get(section, 'boardconfig').lower()
+				except:
+					boardconfig = ''
+				if not boardconfig:
+					boardconfig = self.getBoardConfig(identifier)
 
-                newDevices.append({'deviceName': name, 'deviceID': identifier, 'boardConfig': boardconfig, 'deviceECID': ecid, 'blobsSaved': '[]'})
-        else:
-            print('Unable to find devices.ini')
+				newDevices.append({'deviceName': name, 'deviceID': identifier, 'boardConfig': boardconfig, 'deviceECID': ecid, 'blobsSaved': '[]'})
+		else:
+			print('Unable to find devices.ini')
 
-        # Add only new devices to database
-        for newDevice in newDevices:
-            print('Device: [{deviceName}] ECID: [{deviceECID}] Board Config: [{boardConfig}]'.format(**newDevice))
-            if not db.find_one(deviceECID=newDevice['deviceECID']):
-                numNew += 1
-                db.insert(newDevice)
-        print('Added {} new devices to the database'.format(str(numNew)))
+		# Add only new devices to database
+		for newDevice in newDevices:
+			if not db.find_one(deviceECID=newDevice['deviceECID']):
+				print('Device: [{deviceName}] ECID: [{deviceECID}] Board Config: [{boardConfig}]'.format(**newDevice))
+				numNew += 1
+				db.insert(newDevice)
+		print('Added {} new devices to the database'.format(str(numNew)))
 
-        return
+		return
 
-    def getBoardConfig(self, deviceID):
-        """ Using the IPSW.me API, when supplied a device identifier
-        the relevant board config will be returned. The request to
-        IPSW.me will be stored as `self.liveFirmwareAPI` to avoid
-        unneeded repeated calls to the IPSW.me API. """
+	def getBoardConfig(self, deviceID):
+		""" Using the IPSW.me API, when supplied a device identifier
+		the relevant board config will be returned."""
 
-        if not self.liveFirmwareAPI:
-            self.liveFirmwareAPI = self.removeUnsignedFirmwares(r.get(self.liveFirmwareURL))
-        return self.liveFirmwareAPI[deviceID]['BoardConfig']
+		return self.liveFirmwareAPI[deviceID]['BoardConfig']
 
-    def checkForBlobs(self, deviceECID, buildID):
-        """ Checks against our database to see if blobs for a
-        device have already been saved for a specific iOS version.
-        The device is identified by a deviceECID, iOS version is
-        identified by a buildID. """
+	def checkForBlobs(self, deviceECID, buildID):
+		""" Checks against our database to see if blobs for a
+		device have already been saved for a specific iOS version.
+		The device is identified by a deviceECID, iOS version is
+		identified by a buildID. """
 
-        deviceInfo = self.database['devices'].find_one(deviceECID=deviceECID)
+		deviceInfo = self.database['devices'].find_one(deviceECID=deviceECID)
 
-        for entry in json.loads(deviceInfo['blobsSaved']):
-            if entry['buildID'] == buildID:
-                return True
+		for entry in json.loads(deviceInfo['blobsSaved']):
+			if entry['buildID'] == buildID:
+				return True
 
-        return False
+		return False
 
-    def removeUnsignedFirmwares(self, rawResponse):
-        """ Taking the raw response from the IPSW.me API, process
-         the response as a JSON object and remove unsigned firmware
-         entries. Returns a freshly processed devices JSON containing
-         only signed firmware versions. """
+	def getFirmwareAPI(self):
+		""" Taking the raw response from the IPSW.me API, process
+		 the response as a JSON object and remove unsigned firmware
+		 entries. Returns a freshly processed devices JSON containing
+		 only signed firmware versions. """
 
-        deviceAPI = rawResponse.json()['devices']
+		rawResponse = r.get('https://api.ipsw.me/v2.1/firmwares.json/condensed')
 
-        ''' Rather than messing around with copies, we can loop
-         through all firmware dictionary objects and append the
-         signed firmware objects to a list. The original firmware
-         list is then replaced with the new (signed firmware only) list.'''
-        for deviceID in deviceAPI:
-            signedFirmwares = []
-            for firmware in deviceAPI[deviceID]['firmwares']:
-                if firmware['signed']:
-                    signedFirmwares.append(firmware)
-            deviceAPI[deviceID]['firmwares'] = signedFirmwares
+		deviceAPI = rawResponse.json()['devices']
 
-        return deviceAPI
+		''' Rather than messing around with copies, we can loop
+		 through all firmware dictionary objects and append the
+		 signed firmware objects to a list. The original firmware
+		 list is then replaced with the new (signed firmware only) list.'''
+		for deviceID in deviceAPI:
+			signedFirmwares = []
+			for firmware in deviceAPI[deviceID]['firmwares']:
+				if firmware['signed']:
+					signedFirmwares.append(firmware)
+			deviceAPI[deviceID]['firmwares'] = signedFirmwares
 
-    def checkAllDevices(self):
-        """ Loop through all of our devices and grab matching
-        device firmwares from the firmwareAPI. Device and
-        firmware info is sent to saveBlobs(). """
+		return deviceAPI
 
-        print('\nSaving unsaved blobs for {} devices...'.format(str(len(self.devices))))
-        for device in self.devices:
-            for firmware in self.liveFirmwareAPI[device['deviceID']]['firmwares']:
-                self.saveBlobs(device, firmware['buildid'], firmware['version'])
+	def checkAllDevices(self):
+		""" Loop through all of our devices and grab matching
+		device firmwares from the firmwareAPI. Device and
+		firmware info is sent to saveBlobs(). """
 
-        print('Done saving blobs')
+		print('\nGrabbing devices from the database...')
+		self.devices = [row for row in self.database['devices']]
+		for device in self.devices:
+			print('Device: [{deviceName}] ECID: [{deviceECID}] Board Config: [{boardConfig}]'.format(**device))
+		print('Grabbed {} devices from the database'.format(len(self.devices)))
 
-    def saveBlobs(self, device, buildID, versionNumber):
-        """ First, check to see if blobs have already been
-        saved. If blobs have not been saved, use subprocess
-        to call the tsschecker script and save blobs. After
-        saving blobs, logSavedBlobs() is called to log that
-        we saved the device/firmware blobs. """
 
-        if self.checkForBlobs(device['deviceECID'], buildID):
-            return True
+		print('\nSaving unsaved blobs for {} devices...'.format(str(len(self.devices))))
+		for device in self.devices:
+			for firmware in self.liveFirmwareAPI[device['deviceID']]['firmwares']:
+				self.saveBlobs(device, firmware['buildid'], firmware['version'])
 
-        print('Device: [{}] Version: [{}]'.format(device['deviceID'], versionNumber))
-        savePath = 'blobs/' + device['deviceID'] + '/' + device['deviceECID'] + '/release/' + versionNumber
-        if not os.path.exists(savePath):
-            os.makedirs(savePath)
 
-        tssCall = subprocess.Popen(['./tsschecker_macos',
-                                    '-d', device['deviceID'],
-                                    '-e', device['deviceECID'],
-                                    '--boardconfig', device['boardConfig'],
-                                    '--buildid', buildID,
-                                    '--save-path', savePath,
-                                    '-s'], stdout=subprocess.PIPE)
+		print('Done saving blobs')
 
-        # The io module is a bit overkill but helps easily handle the stream from tssCall
-        tssOutput = []
-        for line in io.TextIOWrapper(tssCall.stdout, encoding='utf-8'):
-            tssOutput.append(line.strip())
+	def saveBlobs(self, device, buildID, versionNumber):
+		""" First, check to see if blobs have already been
+		saved. If blobs have not been saved, use subprocess
+		to call the tsschecker script and save blobs. After
+		saving blobs, logSavedBlobs() is called to log that
+		we saved the device/firmware blobs. """
 
-        ''' Checks console output for the `Saved shsh blobs!`
-        string. While this works for now, tsschecker updates
-        may break the check. It may be possible to check to
-        see if the .shsh file was created and also check for
-        the right file format. '''
-        if 'Saved shsh blobs!' in tssOutput:
-            self.logBlobsSaved(device, buildID, versionNumber)
-            return True
-        else:
-            return False
+		if self.checkForBlobs(device['deviceECID'], buildID):
+			# print('[{0}] [{1}] {2}'.format(device['deviceID'], versionNumber, 'Blobs already saved!'))
+			return
 
-    def logBlobsSaved(self, device, buildID, versionNumber):
-        """ Taking a reference to a device dictionary, we can
-         load the string `blobsSaved` from the database into
-         a JSON object, append a newly saved version, and
-         turn the JSON object back into a string and
-         replace `blobsSaved` """
+		savePath = 'blobs/' + device['deviceID'] + '/' + device['deviceECID'] + '/' + versionNumber + '/' + buildID
+		if not os.path.exists(savePath):
+			os.makedirs(savePath)
 
-        oldBlobsSaved = json.loads(device['blobsSaved'])
-        newBlobsSaved = {'releaseType': 'release', 'versionNumber': versionNumber, 'buildID': buildID}
+		scriptArguments = [self.scriptPath,
+						'-d', device['deviceID'],
+						'-e', device['deviceECID'],
+						'--boardconfig', device['boardConfig'],
+						'--buildid', buildID,
+						'--save-path', savePath,
+						'-s']
 
-        oldBlobsSaved.append(newBlobsSaved)
+		tssCall = subprocess.Popen(scriptArguments, stdout=subprocess.PIPE)
 
-        device['blobsSaved'] = json.dumps(oldBlobsSaved)
+		# The io module is a bit overkill but helps easily handle the stream from tssCall
+		tssOutput = []
+		for line in io.TextIOWrapper(tssCall.stdout, encoding='utf-8'):
+			tssOutput.append(line.strip())
 
-        return
+		''' Checks console output for the `Saved shsh blobs!`
+		string. While this works for now, tsschecker updates
+		may break the check. It may be possible to check to
+		see if the .shsh file was created and also check for
+		the right file format. '''
+		if 'Saved shsh blobs!' in tssOutput:
+			self.logBlobsSaved(device, buildID, versionNumber)
+			print('[{0}] [{1} - {2}] {3}'.format(device['deviceName'], versionNumber, buildID, 'Saved shsh blobs!'))
+		else:
+			self.logBlobsFailed(scriptArguments, savePath, tssOutput)
+			print('[{0}] [{1} - {2}] {3}'.format(device['deviceName'], versionNumber, buildID, 'Error, see log file: ' + '/blobs/' + savePath + '/tsschecker_log.txt'))
 
-    def pushToDatabase(self):
-        """ Loop through all of our devices and update their
-        entries into the database. ECID is used as the value
-        to update by, as it is the only unique device identifier."""
+		return
 
-        print('\nUpdating database with newly saved blobs...')
-        for device in self.devices:
-            self.database['devices'].update(device, ['deviceECID'])
-        print('Done updating database')
+	def logBlobsSaved(self, device, buildID, versionNumber):
+		""" Taking a reference to a device dictionary, we can
+		 load the string `blobsSaved` from the database into
+		 a JSON object, append a newly saved version, and
+		 turn the JSON object back into a string and
+		 replace `blobsSaved` """
 
-        return
+		oldBlobsSaved = json.loads(device['blobsSaved'])
+		newBlobsSaved = {'releaseType': 'release', 'versionNumber': versionNumber, 'buildID': buildID}
+
+		oldBlobsSaved.append(newBlobsSaved)
+
+		device['blobsSaved'] = json.dumps(oldBlobsSaved)
+
+		return
+
+	def logBlobsFailed(self, scriptArguments, savePath, tssOutput):
+		""" When blobs are unable to be saved, we save
+		a log of tsschecker's output in the blobs folder. """
+
+		with open(savePath + '/tsschecker_log.txt', 'w') as file:
+			file.write(' '.join(scriptArguments) + '\n\n')
+			file.write('\n'.join(tssOutput))
+
+		return
+
+	def pushToDatabase(self):
+		""" Loop through all of our devices and update their
+		entries into the database. ECID is used as the value
+		to update by, as it is the only unique device identifier."""
+
+		print('\nUpdating database with newly saved blobs...')
+		for device in self.devices:
+			self.database['devices'].update(device, ['deviceECID'])
+		print('Done updating database')
+
+		return
+
+	def getScriptPath(self, userPath):
+		""" Determines if the user provided a path to the tsschecker
+		 binary, whether command line argument or passed to autotss().
+		 If the user did not provide a path, try to find it within
+		 /tsschecker or /tsschecker-latest and select the proper binary
+		 Also verifies that these files exist. """
+
+		scriptPath = None
+
+		argParser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+		argParser.add_argument("-p", "--path", help='Supply the path to your tsschecker binary.\nExample: -p /Users/codsane/tsschecker/tsschecker_macos', required=False, default='')
+		argument = argParser.parse_args()
+
+		# Check to see if the user provided the command line argument -p or --path
+		if argument.path:
+			scriptPath = argument.path
+
+			# Check to make sure this file exists
+			if os.path.isfile(argument.path):
+				print('Using manually specified tsschecker binary: ' + argument.path)
+			else:
+				print('Unable to find tsschecker at specificed path: ' + argument.path)
+				sys.exit()
+
+		# No command line argument provided, check to see if a path was passed to autotss()
+		else:
+			if userPath:
+				scriptPath = userPath
+
+				# Check to make sure this file exists
+				if os.path.isfile(userPath):
+					print('Using manually specified tsschecker binary: ' + userPath)
+				else:
+					print('Unable to find tsschecker at specificed path: ' + userPath)
+					sys.exit()
+
+			# No path was passed to autotss(), try and find tsschecker within /tsschecker or /tsschecker-latest
+			else:
+				if sys.platform == "linux" or sys.platform == "linux2":
+					scriptName = 'tsschecker_linux'
+				elif sys.platform == "darwin":
+					scriptName = 'tsschecker_macos'
+				elif sys.platform == "win32" or "win64":
+					scriptName = 'tsschecker_windows.exe'
+
+				# Check to make sure this file exists
+				theIdealPath = os.path.join('tsschecker', scriptName)
+				theOtherOne = os.path.join('tsschecker-latest', scriptName)
+				if os.path.isfile(theIdealPath):
+					scriptPath = theIdealPath
+				elif os.path.isfile(theOtherOne):
+					scriptPath = theOtherOne
+				else:
+					print('\nCould not find the tsschecker binary.')
+
+					''' tsschecker releases haven't included a Windows binary in quite
+					a while. We check to see if one exists however warn the user that
+					they may have to build one themselves. '''
+					if scriptName == 'tsschecker_windows.exe':
+						print('Recent tsschecker builds have not included a Windows binary, you may need to build one yourself')
+
+					print('Get tsschecker here: http://api.tihmstar.net/builds/tsschecker/tsschecker-latest.zip')
+					print('Unzip into the same folder as autotss')
+					sys.exit()
+
+				print('Automatically found tsschecker binary: ' + scriptPath)
+
+		return scriptPath
 
 def main():
-    autotss()
+	# autotss('/Users/codsane/tsschecker/tsschecker_macos')
+	autotss()
 
 if __name__ == "__main__":
-    main()
+	main()
